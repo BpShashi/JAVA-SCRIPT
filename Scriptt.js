2df762c3d09d80bfa0998b2425f1ded4e4c5bf20
BpShashi <shashibp2001@gmail.com>
2024-09-27 11:22:53 +0530

    first commit

---
+++ Scriptt.js
@@ -0,0 +1,28 @@
+// const redDiv = document.getElementById("red");
+// const greenDiv = document.getElementById("green");
+// const blueDiv = document.getElementById("blue");
+
+// redDiv.addEventListener("contextmenu", (event)=>{
+//     event.preventDefault();
+//     alert("red clicked");
+
+// }, true)
+// greenDiv.addEventListener("click", ()=>{
+//     alert("green clicked");
+// }, true)
+
+// blueDiv.addEventListener("click", (event) =>{
+//     //event.stopPropagation();
+//     alert("blue clicked");
+// }, true)
+
+// let sum =0;
+// const number = [65,44,12,4];
+// number.foreach(myfuction);
+
+// function myfuction(item){
+//         sum +=item;
+// }
+
+
+let fournumber =[]
